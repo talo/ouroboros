b207c806b8ddea86971400de59829b6478584143
Loong <bzlw@pm.me>
2024-03-18 14:05:05 +0800

    chore: ptr tests

--- ouroboros/src/ptr.rs
+++ ouroboros/src/ptr.rs
@@ -40,3 +40,67 @@ impl Display for Ptr {
         self.t.fmt(f)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{Record, TypeInfo};
+
+    use super::*;
+
+    #[test]
+    fn test_ptr_display() {
+        assert_eq!(Ptr::new(<()>::t()).to_string(), "@()");
+        assert_eq!(Ptr::new(i32::t()).to_string(), "@i32");
+        assert_eq!(Ptr::new(u32::t()).to_string(), "@u32");
+        assert_eq!(Ptr::new(f32::t()).to_string(), "@f32");
+        assert_eq!(Ptr::new(<Vec<i32>>::t()).to_string(), "@[i32]");
+    }
+
+    #[test]
+    fn test_ptr_compat() {
+        assert_eq!(
+            Ptr::new(<()>::t()).is_compat(Some(&serde_json::Value::String(
+                "any-kind-of-ptr-value-goes-here".into()
+            ))),
+            Ok(())
+        );
+        assert_eq!(
+            Ptr::new(<()>::t()).is_compat(Some(&serde_json::Value::Null)),
+            Err(Error::InvalidPtr {
+                expected: Ptr::new(<()>::t()),
+                e: Error::UnexpectedValue {
+                    got: serde_json::Value::Null
+                }
+                .into(),
+            })
+        );
+        assert_eq!(
+            Ptr::new(<()>::t()).is_compat(Some(&serde_json::Value::Bool(false))),
+            Err(Error::InvalidPtr {
+                expected: Ptr::new(<()>::t()),
+                e: Error::UnexpectedValue {
+                    got: serde_json::Value::Bool(false)
+                }
+                .into(),
+            })
+        );
+        assert_eq!(
+            Ptr::new(<()>::t()).is_compat(None),
+            Err(Error::InvalidPtr {
+                expected: Ptr::new(<()>::t()),
+                e: Error::UnexpectedNull.into(),
+            })
+        );
+
+        assert_eq!(
+            Ptr::new(Type::Record(Record::new(
+                "Foo",
+                [("x", i32::t()), ("y", u32::t()), ("z", f32::t())]
+            )))
+            .is_compat(Some(&serde_json::Value::String(
+                "any-kind-of-ptr-value-goes-here".into()
+            ))),
+            Ok(())
+        );
+    }
+}
